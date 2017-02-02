0e7481ac3eb28cb955ad1c7a8720af87fb6e7a0a
lucncdm <lucncdm@gmail.com>
2017-02-02 11:26:39 +0100

    changes

--- tsinalis.py
+++ tsinalis.py
@@ -53,6 +53,15 @@ ages_tensor = tf.nn.relu(tf.matmul(h_fc1, W5) + b5)
 euc_distance = tf.reduce_mean(tf.square(ages_tensor - true_ages))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(euc_distance)
 
+first_patient = 0
+
+def forward_batch(n, sess, eegs, ages):
+    global first_patient
+    total = eegs.shape[0]
+    feed = {eegs_: eegs[first_patient:first_patient+n], true_ages:ages[first_patient:first_patient+n]}
+    first_patient = (first_patient+n)%total
+    agest, dist, _ = sess.run((ages_tensor, euc_distance, train_step), feed_dict=feed)
+    #print(dist, agest)
 
 def slice_eeg(eeg, size, hop):
     out = []
@@ -60,9 +69,36 @@ def slice_eeg(eeg, size, hop):
         out.append(eeg[i*hop:i*hop+size])
     return out
 
+def eval1patient(sess, eeg):
+    slices = np.array(slice_eeg(eeg, 15000, 5000))
+    ans = sess.run(ages_tensor, {eeg:slices})
+    return np.mean(ans), np.std(ans)
+    #print("CNN; score: {}".format(mape(ans, ages)))
 
+def eval_all(sess, eegs, ages):
+    ages_c = np.zeros(ages.size)
+    stds = np.zeros(ages.size)
+    for i, s in enumerate(eegs):
+        ages_c[i], stds[i] = eval1patient(sess, s)
+    print("CNN; score: {}, std: {}".format(mape(ages_c, ages), np.mean(stds)))
 
-a = np.ones((50,15000))
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-sess.run(train_step, {eeg:a, true_ages:np.ones(50)})
+def slice_and_stack(eegs, size, hop):
+	out = []
+	for s in eegs:
+	    slices = slice_eeg(s, size, hop)
+	    out += slices
+	return np.array(out)
+
+if __name__ == "__main__":
+    train_hyp, train_eegs, train_devices, train_labels, eval_hyp, eval_eegs, eval_devices, eval_labels = train_eval_base()
+    t_slices = slice_and_stack(train_eegs, 15000, 5000)
+    ages = train_labels
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    print(first_patient)
+    for k in range(10000):
+        #forward_batch(50, sess, specs, train_feat, train_labels)
+        forward_batch(50, sess, t_slices, train_labels)
+        if k % 100 == 0:
+            #forward_eval(sess, eval_specs, eval_feat, eval_labels)
+            eval_all(sess, eval_eegs, eval_labels)
